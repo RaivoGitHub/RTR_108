cbd5dccd229a55d2a073f089bc7366920ccc9d47
user <Tatjana.Solovjova@rtu.lv>
2020-02-13 14:10:36 +0200

    test_13_02_2020.py

---
+++ test.py
@@ -0,0 +1,14 @@
+#a = 10
+
+#vards='Raivo'
+#uzvards='Skripka'
+#print("Vards, uzvards: %s %s "%(vards,uzvards))
+
+
+#x = 45
+#y = 10
+#print("%d * %d = %d"%(x,y,x*y))
+
+a = input("Ievadi pirmo skaitli: ")
+b = input("Ievadi otro skaitli: ")
+print("Iznakums: \n %d"%(int(a)*int(b)))
